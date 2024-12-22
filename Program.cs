02bcc0c374687fc79dfab5c334fd8922b2972225
RandomiaGaming <randomiagaming@gmail.com>
2024-12-22 01:56:06 -0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person2PrintOut
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public static void SplitIntoPages(float height, Bitmap image)
+        {
+            float widthInches = image.Width * pixelsPerInch;
+            float heightInches = image.Height * pixelsPerInch;
+
+        }
+    }
+}
